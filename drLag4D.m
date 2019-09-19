8fec9d00035f62b8f68ef4f7fa41fb3a1304a75b
Toshihiko ASO <55236088+aso-toshihiko@users.noreply.github.com>
2019-09-19 16:19:07 +0900

    Update drLag4D.m

--- drLag4D.m
+++ drLag4D.m
@@ -12,15 +12,15 @@ function dir = drLag4D( name, TR, vols, PosiMax, THR, FIXED, range)
 %			Note this parameter affects the bandpass filter. 
 %	THR: Minimum height of the valid cross-correlogram peak for lag mapping
 %	FIXED: Specify tracking method. 
-			0 = recursive tracking, nonzero = fixed seed tracking
+%			0 = recursive tracking, nonzero = fixed seed tracking
 %	range: Specify fourth dimension (time) of the 4D data to be used.
 %			ex. 1:500 - use first 300 volumes
 %			ex.	1:2:500 - "decimate" to see the effect of sampling rate (see Aso 2017; don't forget to double the TR!)
 %
 %	References
 %	Aso, T., Jiang, G., Urayama, S. I., & Fukuyama, H. (2017). A resilient, non-neuronal source of the spatiotemporal lag structure detected by bold signal-based blood flow tracking. Frontiers in Neuroscience, 11(MAY), 1-13. https://doi.org/10.3389/fnins.2017.00256
-%	Satow, T., Aso, T., Nishida, S., Komuro, T., Ueno, T., Oishi, N., … Fukuyama, H. (2017). Alteration of venous drainage route in idiopathic normal pressure hydrocephalus and normal aging. Frontiers in Aging Neuroscience, 9(NOV), 1-10. https://doi.org/10.3389/fnagi.2017.00387
-%	Nishida, S., Aso, T., Takaya, S., Takahashi, Y., Kikuchi, T., Funaki, T., … Miyamoto, S. (2018). Resting-state Functional Magnetic Resonance Imaging Identifies Cerebrovascular Reactivity Impairment in Patients With Arterial Occlusive Diseases: A Pilot Study. Neurosurgery, 0(0), 1-9. https://doi.org/10.1093/neuros/nyy434
+%	Satow, T., Aso, T., Nishida, S., Komuro, T., Ueno, T., Oishi, N., ﾂ… Fukuyama, H. (2017). Alteration of venous drainage route in idiopathic normal pressure hydrocephalus and normal aging. Frontiers in Aging Neuroscience, 9(NOV), 1-10. https://doi.org/10.3389/fnagi.2017.00387
+%	Nishida, S., Aso, T., Takaya, S., Takahashi, Y., Kikuchi, T., Funaki, T., ﾂ… Miyamoto, S. (2018). Resting-state Functional Magnetic Resonance Imaging Identifies Cerebrovascular Reactivity Impairment in Patients With Arterial Occlusive Diseases: A Pilot Study. Neurosurgery, 0(0), 1-9. https://doi.org/10.1093/neuros/nyy434
 %	(in press) Aso, T., Urayama, S., Hidenao, F., & Murai, T. (2019). Axial variation of deoxyhemoglobin density as a source of the low-frequency time lag structure in blood oxygenation level-dependent signals. BioRxiv, 658377. https://doi.org/10.1101/658377
 
 setenv('FSLOUTPUTTYPE', 'NIFTI'); % this to tell what the output type would be
